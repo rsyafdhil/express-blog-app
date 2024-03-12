98c94972c36d1f8808b4e3aa23a4cca902ffe7a9
Rassya <rasyafadhil@gmail.com>
2024-03-12 11:15:15 +0700

    feature: add details blog pages :sparkles:

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const moment = require('moment');
 
 const app = express();
 const port = 3000;
@@ -32,9 +33,21 @@ app.get('/create', (req, res) => {
     res.render('create.ejs');
 });
 
+app.get('/:title', (req, res) => {
+    const reqTitle = req.params.title;
+
+    submittedData.forEach((data) => {
+        if (data.title === reqTitle) {
+            res.render('post.ejs', {title: data.title, content: data.content, author: data.author, date: data.created_at});
+        }
+    }
+    );
+});
+
 app.post('/submit', (req, res) => {
+    const created_at = moment().format('ll');
     const {title, content, author} = req.body;
-    submittedData.push({title, content, author});
+    submittedData.push({title, content, author, created_at});
 
     console.log(JSON.stringify(submittedData));
 
